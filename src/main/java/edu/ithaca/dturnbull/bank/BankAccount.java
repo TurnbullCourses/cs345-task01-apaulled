e3261df25c9acb26b5cc3e1883af8dc0a165118b
WhirlwindWar <gaglianopa@gmail.com>
2025-01-28 12:22:03 -0500

    replace broken implementations until they can be fixed by Kat

--- src/main/java/edu/ithaca/dturnbull/bank/BankAccount.java
+++ src/main/java/edu/ithaca/dturnbull/bank/BankAccount.java
@@ -1,14 +1,18 @@
 package edu.ithaca.dturnbull.bank;
 
+import java.util.List;
+
 public class BankAccount {
 
     private String email;
     private double balance;
 
+    // FYI: I commented out some of these methods and replaced them with my own for the sake of the assignment. Kat, if you want to uncomment the originals and finish it feel free.
+
     /**
      * @throws IllegalArgumentException if email is invalid
      */
-    public BankAccount(String email, double startingBalance){
+    /*public BankAccount(String email, double startingBalance){
         if (isEmailValid(email)){
             this.email = email;
             this.balance = startingBalance;
@@ -16,6 +20,13 @@ public class BankAccount {
         else {
             throw new IllegalArgumentException("Email address: " + email + " is invalid, cannot create account");
         }
+    }*/
+
+    public BankAccount(String email, double startingBalance) {
+        if (!isEmailValid(email)) throw new IllegalArgumentException("Email address: " + email + " is invalid, cannot create account");
+        if (!isAmountValid(startingBalance)) throw new IllegalArgumentException("Starting balance: " + startingBalance + " is invalid");
+        this.email = email;
+        this.balance = startingBalance;
     }
 
     public double getBalance(){
@@ -27,10 +38,12 @@ public class BankAccount {
     }
 
     /**
+     * @param amount the amount of money to withdraw
      * @post reduces the balance by amount if amount is non-negative and smaller than balance
      * @throws InsufficientFundsException if amount is negative or greater than balance
      */
     public void withdraw (double amount) throws InsufficientFundsException{
+        if (!isAmountValid(amount)) throw new IllegalArgumentException("Amount: " + amount + " is invalid"); 
         if (amount <= balance){
             balance -= amount;
         }
@@ -43,12 +56,18 @@ public class BankAccount {
      * @post increases the balance by amount if amount is non-negative
      */
     public void deposit (double amount) throws InsufficientFundsException{
+        if (!isAmountValid(amount)) throw new IllegalArgumentException("Amount: " + amount + " is invalid"); 
+        balance += amount;
     }
 
     /**
+     * @param other the account to transfer money to
+     * @param amount the amount of money to transfer
      * @post increases the balance of the other account and decreases the balance of this account by the amount if it is non-negative and less than the balance of this account
      */
     public void transfer (BankAccount other, double amount) throws InsufficientFundsException{
+        withdraw(amount);
+        other.deposit(amount);
     }
 
         /**
@@ -61,7 +80,40 @@ public class BankAccount {
         return true;
     }
 
+    // FYI: I replaced isEmailValid with my implementation so that the tests would pass. Kat, if you want to uncomment your version and finish it feel free
+
+    private static boolean checkPart(String part, List<Character> allowedPunctuation) {
+        if (allowedPunctuation.contains(part.charAt(0)) || allowedPunctuation.contains(part.charAt(part.length()-1))) return false;
+        for (int i = 0; i < part.length(); i++) {
+            if (!Character.isLetterOrDigit(part.charAt(i)) && !allowedPunctuation.contains(part.charAt(i))) return false;
+            if (allowedPunctuation.contains(part.charAt(i)) && allowedPunctuation.contains(part.charAt(i-1))) return false;
+        }
+        return true;
+    }
+
     public static boolean isEmailValid(String email){
+        String[] parts = email.split("@");
+        if (parts.length != 2 || parts[0].length() == 0 || parts[1].length() == 0) return false;
+        List<Character> allowedPunctuation = List.of('-', '.', '_');
+        if (!checkPart(parts[0], allowedPunctuation)) return false;
+
+        String[] domainParts = parts[1].split("\\.");
+        if (domainParts.length != 2 || domainParts[0].length() == 0 || domainParts[1].length() == 0) return false;
+
+        List<Character> allowedPunctuationDomain = List.of('-');
+        if (!checkPart(domainParts[0], allowedPunctuationDomain)) return false;
+
+        if (domainParts[1].length() < 2) return false;
+
+        if (!checkPart(domainParts[1], allowedPunctuationDomain)) return false;
+        return true;
+    }
+
+    /*
+     * checks the validity of an email address
+     * @param email the email address to check
+     */
+    /*public static boolean isEmailValid(String email){
         if (email.indexOf('@') == -1){
             return false;
         }
@@ -71,5 +123,5 @@ public class BankAccount {
         else {
             return true;
         }
-    }
+    }*/
 }
